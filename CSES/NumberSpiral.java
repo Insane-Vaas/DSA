79e64308f9f23e6bb128d29f8a611be4471c6148
Insane-Vaas <hrithikr200@gmail.com>
2024-12-12 21:22:30 +0530

    CSES Number Spiral Solution

--- CSES/NumberSpiral.java
+++ CSES/NumberSpiral.java
@@ -1,33 +1,152 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class NumberSpiral {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        FastScanner scan = new FastScanner();
         Long n = scan.nextLong();
         StringBuilder str = new StringBuilder();
 
         while (n > 0) {
             long x = scan.nextLong();
             long y = scan.nextLong();
-
-            if (x == 1 || y == 1) {
-                str.append(
-                    (x * x > y * y)
-                        ? Long.toString(x * x) + "\n"
-                        : Long.toString(y * y) + "\n"
-                );
+            long r = 0;
+            if (x < y) {
+                if (y % 2 == 0) r = (y - 1) * (y - 1) + x;
+                else r = y * y - x + 1;
             } else {
-                str.append(
-                    (x * x >= y * y)
-                        ? Long.toString(x * x - y + 1) + "\n"
-                        : Long.toString(y * y - x + 1) + "\n"
-                );
+                if (x % 2 == 0) r = x * x - y + 1;
+                else r = (x - 1) * (x - 1) + y;
             }
+
+            System.out.println(r);
             n--;
         }
-        System.out.println(str);
-
         scan.close();
+    }
+}
+
+final class FastScanner {
+
+    private int BS = 1 << 16;
+    private char NC = (char) 0;
+    private byte[] buf = new byte[BS];
+    private int bId = 0, size = 0;
+    private char c = NC;
+    private double cnt = 1;
+    private BufferedInputStream in;
+
+    public FastScanner() {
+        in = new BufferedInputStream(System.in, BS);
+    }
+
+    public FastScanner(String s) {
+        try {
+            in = new BufferedInputStream(new FileInputStream(new File(s)), BS);
+        } catch (Exception e) {
+            in = new BufferedInputStream(System.in, BS);
+        }
+    }
+
+    private char getChar() {
+        while (bId == size) {
+            try {
+                size = in.read(buf);
+            } catch (Exception e) {
+                return NC;
+            }
+            if (size == -1) return NC;
+            bId = 0;
+        }
+        return (char) buf[bId++];
+    }
+
+    public int nextInt() {
+        return (int) nextLong();
+    }
+
+    public int[] nextInts(int N) {
+        int[] res = new int[N];
+        for (int i = 0; i < N; i++) {
+            res[i] = (int) nextLong();
+        }
+        return res;
+    }
+
+    public long[] nextLongs(int N) {
+        long[] res = new long[N];
+        for (int i = 0; i < N; i++) {
+            res[i] = nextLong();
+        }
+        return res;
+    }
+
+    public long nextLong() {
+        cnt = 1;
+        boolean neg = false;
+        if (c == NC) c = getChar();
+        for (; (c < '0' || c > '9'); c = getChar()) {
+            if (c == '-') neg = true;
+        }
+        long res = 0;
+        for (; c >= '0' && c <= '9'; c = getChar()) {
+            res = (res << 3) + (res << 1) + c - '0';
+            cnt *= 10;
+        }
+        return neg ? -res : res;
+    }
+
+    public double nextDouble() {
+        double cur = nextLong();
+        return c != '.' ? cur : cur + nextLong() / cnt;
+    }
+
+    public double[] nextDoubles(int N) {
+        double[] res = new double[N];
+        for (int i = 0; i < N; i++) {
+            res[i] = nextDouble();
+        }
+        return res;
+    }
+
+    public String next() {
+        StringBuilder res = new StringBuilder();
+        while (c <= 32) c = getChar();
+        while (c > 32) {
+            res.append(c);
+            c = getChar();
+        }
+        return res.toString();
+    }
+
+    public String nextLine() {
+        StringBuilder res = new StringBuilder();
+        while (c <= 32) c = getChar();
+        while (c != '\n') {
+            res.append(c);
+            c = getChar();
+        }
+        return res.toString();
+    }
+
+    public boolean hasNext() {
+        if (c > 32) return true;
+        while (true) {
+            c = getChar();
+            if (c == NC) return false;
+            else if (c > 32) return true;
+        }
+    }
+
+    public void close() throws Exception {
+        in.close();
+    }
+
+    public static void sort(int[] arr) {
+        ArrayList<Integer> ls = new ArrayList<Integer>();
+        for (int x : arr) ls.add(x);
+        Collections.sort(ls);
+        for (int i = 0; i < arr.length; i++) arr[i] = ls.get(i);
     }
 }
